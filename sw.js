d494a0ca49a19c52c7ec25ee5911b4e6948c766a
wdwedqwdkhlbvdgwedyieh <At412014@gmail.com>
2026-06-03 12:27:50 +0100

    Make it offlin upoe

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const CACHE_NAME = 'nexo-cache-v1';
+const CACHE_NAME = 'nexo-cache-v2';
+// حيدنا الخطوط ديال ڭوڭل باش ما يديروش لينا مشكل فالتخزين
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json',
-  // الخطوط اللي مستعمل في الكود باش الموقع يبقى بنفس الديزاين أوفلاين
-  'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,700;0,900;1,400&family=JetBrains+Mono:wght@400;600&family=Outfit:wght@300;400;500;600&display=swap'
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
@@ -18,7 +17,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // إيلا كان الملف مخبي، عطيه ليه، وإيلا لا جيبو من الأنترنيت
         return response || fetch(event.request);
       })
   );
