6490403266f585b8bfb000e7db04bd11c668e2f4
wdwedqwdkhlbvdgwedyieh <At412014@gmail.com>
2026-06-03 12:36:01 +0100

    Make it offlin uspoe

--- sw.js
+++ sw.js
@@ -1,23 +1,39 @@
-const CACHE_NAME = 'nexo-cache-v2';
-// حيدنا الخطوط ديال ڭوڭل باش ما يديروش لينا مشكل فالتخزين
+const CACHE_NAME = 'nexo-cache-v3';
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json'
 ];
 
+// هادشي كيفرض على Service Worker يتانصطالا فالبلاصة
 self.addEventListener('install', event => {
+  self.skipWaiting(); 
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// هادشي كيمسح أي كاش قديم خاسر 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// هادشي باش إيلا تقطعات الكونيكسيون يرجعك ديريكت لـ index.html
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => caches.match('/index.html'));
+    })
   );
 });
